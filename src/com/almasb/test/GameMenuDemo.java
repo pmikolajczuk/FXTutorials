fdfaefe6e7ac03539e971d9b00e4ec65fda9acd8
pmikolajczuk <pidus@o2.pl>
2017-05-19 17:51:46 +0200

    Signed-off-by: pmikolajczuk <pidus@o2.pl>

--- src/com/almasb/test/GameMenuDemo.java
+++
@@ -1,8 +0,0 @@
-package com.almasb.test;
-
-public class GameMenuDemo{
-
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/com/almasb/tutorial4/GameMenuDemo.java
@@ -0,0 +1,16 @@
+package com.almasb.tutorial4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameMenuDemo extends Application{
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
