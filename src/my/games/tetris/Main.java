a267a98eeaf390782391041a46e7d3604073fc38
pmikolajczuk <pidus@o2.pl>
2022-06-27 20:29:25 +0200

    Tetris game name

--- src/my/games/tetris/Main.java
+++ src/my/games/tetris/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Pong game!");
+        primaryStage.setTitle("Tetris game!");
 
         StackPane root = new StackPane();
         Scene scene = new Scene(root);
